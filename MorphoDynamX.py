9021651dadc9f9f2310bbb16ef226cc99d00095c
Richard S. Smith <Richard.Smith@jic.ac.uk>
2024-12-09 10:35:08 +0000

    Added the values for mesh 1_3, and moved the csv files to the output folder

---
+++ MorphoDynamX.py
@@ -0,0 +1,5 @@
+######## New MorphoDynamX session v2.0 r2-1582: 2024-12-05 10:20:16
+
+Process.Tools__System__Load_View('FemMembranes.mdxv')
+######## New MorphoDynamX session v2.0 r2-1582: 2024-12-05 10:20:56
+Process.Tools__System__Load_View('FemMembranes.mdxv')
